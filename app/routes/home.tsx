d685ff592de8d9b9dbf5bf2caca35908a376edcf
Seno Aji <cydnirn.dev@gmail.com>
2026-05-18 14:37:13 +0700

    fix: Fix overflow x in home

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -19,7 +19,7 @@ const images = [
 
 export default function Home() {
   return (
-    <div className="scroll-smooth">
+    <div className="scroll-smooth overflow-x-hidden">
       <section className="bg-linear-to-b from-[#F8F546] via-[#F8F546] to-white w-full h-[80vh] flex justify-center flex-col items-center cursor-default">
         <div className="helvetica font-extrabold text-[10vw] transition flicker-appearY">
           //ENDFIELD
