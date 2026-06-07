4da12534c56229bc9f2145c0d11dfa11662b47ae
Seno Aji <cydnirn.dev@gmail.com>
2026-06-07 15:01:08 +0700

    feat: Add dashboard basic layout and page

--- app/routes.ts
+++ app/routes.ts
@@ -6,11 +6,14 @@ import {
 } from "@react-router/dev/routes";
 
 export default [
-  layout("routes/home.tsx", [
-    index("routes/home-content.tsx"),
-    route("about", "routes/about.tsx"),
-    route("products", "routes/products/products.tsx"),
-    route("products/:id", "routes/products/productId.tsx"),
-    route("contact", "routes/contact.tsx"),
+  layout("routes/home/home.tsx", [
+    index("routes/home/home-content.tsx"),
+    route("about", "routes/home/about.tsx"),
+    route("products", "routes/home/products/products.tsx"),
+    route("products/:id", "routes/home/products/productId.tsx"),
+    route("contact", "routes/home/contact.tsx"),
+  ]),
+  layout("routes/dashboard/dashboard.tsx", [
+    index("routes/dashboard/login.tsx"),
   ]),
 ] satisfies RouteConfig;
---
+++ app/routes/dashboard/dashboard.tsx
@@ -0,0 +1,9 @@
+import { Fragment } from "react/jsx-runtime";
+
+export default function Dashboard() {
+  return (
+    <Fragment>
+      <p>Hello World</p>
+    </Fragment>
+  );
+}
